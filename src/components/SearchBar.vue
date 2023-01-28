5f9ab6f62de11981880574122d24e38201c9071b
Mason <baboom16mll@gmail.com>
2023-01-28 15:58:47 -0800

    adding basic search bar log dumb

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="text" class="searchinput" v-model="SearchValue" />
+  <button class="searchbutton" @click="SearchHandler">search</button>
+</template>
+
+<script setup>
+import { useStore } from "/src/Store/Store";
+
+const { SearchValue } = useStore();
+
+const SearchHandler = () => {
+  console.log(SearchValue.value);
+};
+</script>
+<style scoped></style>
