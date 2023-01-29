699dd66717996fdbe42c3387d1fdf933040a9e52
Mason <baboom16mll@gmail.com>
2023-01-28 22:32:50 -0800

    implementing changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import SearchBar from "@/components/SearchBar.vue";
-import { useStore } from "@/Store/Store";
-
-const { SearchValue } = useStore();
 </script>
 
 <template>
   <div class="testing">
     <SearchBar />
-    <h1>{{ SearchValue }}</h1>
   </div>
 </template>
 
